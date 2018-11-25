c7af438eee9bd52fbd84bfb8fd85d70e7d949340
shyyhs <songskg6666@gmail.com>
2018-11-25 22:53:39 +0900

    tensorData

--- RNN_Generation/data.py
+++ RNN_Generation/data.py
@@ -13,13 +13,17 @@ from globalVar import *
 # assistant variable, viarable letters
 all_letters = string.ascii_letters + ".,;'-"
 n_letters = len(all_letters)+1 # Plus EOF
+letterN = n_letters
+letterLst = all_letters
 
 # about category, number, type and dictionary
 categoryN = 0
 categoryType= []
 categoryData = {}
 
-# Tese Three functions for readlines into [] and change it to ASCII
+# Tese four functions for readlines into [] and change it to ASCII,
+# Last function getCate() be executed when call it.
+
 def findFiles(path): return glob.glob(path)
 
 def unicodeToAscii(s):
@@ -39,10 +43,40 @@ def getCate():
         categoryType.append(categoryTmp)
         categoryData[categoryTmp]=readLines(filename)
     categoryN = len(categoryType)
+
+# Data for training
+def randomItem(lst):
+    return lst[random.randint(0,len(lst)-1)]
+def randomInputSample():
+    randCate = randomItem(categoryType)
+    randLine = randomItem(categoryData[randCate])
+    return randCate, randLine
+
+#One-hot
+def tensorCategory(category):
+    li = categoryType.index(category)
+    tensor = torch.zeros(1,categoryN)
+    tensor[li] = 1
+    return tensor
+    
+def tensorInput(line):
+    tensor = torch.zeros(len(line),1,letterN)
+    for i in range(len(line)):
+        c = line[i]
+        tensor[i][0][letterLst.index(c)] = 1
+    return tensor
+
+def tensorTarget(line):
+    target = [letterLst.find(c) for c in line[1:]]
+    target.append(letterN-1)
+    return torch.tensor(target, dtype = int64)
+         
+    
         
 if (__name__!="__main__"):
     #This function is used by other functions, not test
     print("Data module")
+    getCate()
 
 
 if (__name__=="__main__"):
